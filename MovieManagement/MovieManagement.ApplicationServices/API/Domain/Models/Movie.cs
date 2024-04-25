dc545ac0c6682500a58f57edd8e9dd613a18e9fd
Greg Zieliński <120790228+Zetix7@users.noreply.github.com>
2024-04-25 18:16:43 +0200

    Created MovieManagement.ApplicationServices project, created Movie domain model

---
+++ MovieManagement/MovieManagement.ApplicationServices/API/Domain/Models/Movie.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieManagement.ApplicationServices.API.Domain.Models;
+
+public class Movie
+{
+    public string? Title { get; set; }
+    public int Year { get; set; }
+    public string? Universe { get; set; }
+    public decimal BoxOffice { get; set; }
+}
