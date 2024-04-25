33bf15b44d6036c393b3a41f7df4431081961902
Greg Zieliński <120790228+Zetix7@users.noreply.github.com>
2024-04-25 18:10:46 +0200

    Created MovieController class, created GetAllMovies and GetMovieById methods as endpoints GET

---
+++ MovieManagement/MovieManagement/Controllers/MovieController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieManagement.DataAccess;
+using MovieManagement.DataAccess.Entities;
+
+namespace MovieManagement.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MovieController : ControllerBase
+{
+    private readonly IRepository<Movie> _movieRepository;
+
+    public MovieController(IRepository<Movie> movieRepository)
+    {
+        _movieRepository = movieRepository;
+    }
+
+    [HttpGet]
+    [Route("")]
+    public IEnumerable<Movie> GetAllMovies()
+    {
+        return _movieRepository.GetAll();
+    }
+
+    [HttpGet]
+    [Route("{movieId}")]
+    public Movie GetMovieById(int movieId)
+    {
+        return _movieRepository.GetById(movieId);
+    }
+}
