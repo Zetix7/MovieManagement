29fb4c4f7309f1ef7124f5e9f1f88cf2d1d0631c
Greg Zieliński <120790228+Zetix7@users.noreply.github.com>
2024-04-26 10:25:40 +0200

    Refactored Handle method in GetActorByIdHandler class

--- MovieManagement/MovieManagement.ApplicationServices/API/Handlers/GetActorByIdHandler.cs
+++ MovieManagement/MovieManagement.ApplicationServices/API/Handlers/GetActorByIdHandler.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using MediatR;
 using MovieManagement.ApplicationServices.API.Domain;
 using MovieManagement.ApplicationServices.API.Domain.Models;
 using MovieManagement.DataAccess;
@@ -8,29 +9,26 @@ namespace MovieManagement.ApplicationServices.API.Handlers;
 public class GetActorByIdHandler : IRequestHandler<GetActorByIdRequest, GetActorByIdResponse>
 {
     private readonly IRepository<DataAccess.Entities.Actor> _actorRepository;
+    private readonly IMapper _mapper;
 
-    public GetActorByIdHandler(IRepository<DataAccess.Entities.Actor> actorRepository)
+    public GetActorByIdHandler(IRepository<DataAccess.Entities.Actor> actorRepository, IMapper mapper)
     {
         _actorRepository = actorRepository;
+        _mapper = mapper;
     }
 
     public Task<GetActorByIdResponse> Handle(GetActorByIdRequest request, CancellationToken cancellationToken)
     {
         var actor = _actorRepository.GetById(request.Id);
-        if(actor is null)
+        if (actor is null)
         {
             return Task.FromResult(new GetActorByIdResponse());
         }
-
-        var domainActor = new Actor
-        {
-            FirstName = actor.FirstName,
-            LastName = actor.LastName
-        };
+        var mappedActor = _mapper.Map<Actor>(actor);
 
         var response = new GetActorByIdResponse
         {
-            Data = domainActor
+            Data = mappedActor
         };
 
         return Task.FromResult(response);
