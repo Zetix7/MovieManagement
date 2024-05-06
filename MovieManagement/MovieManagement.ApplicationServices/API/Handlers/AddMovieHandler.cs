eb5738e9788f943c42250dbbe8e279a46d518acb
Greg Zieliński <120790228+Zetix7@users.noreply.github.com>
2024-05-06 12:28:54 +0200

    Added error service in AddMovie Handler and Command classes

--- MovieManagement/MovieManagement.ApplicationServices/API/Handlers/AddMovieHandler.cs
+++ MovieManagement/MovieManagement.ApplicationServices/API/Handlers/AddMovieHandler.cs
@@ -2,6 +2,7 @@
 using MediatR;
 using MovieManagement.ApplicationServices.API.Domain;
 using MovieManagement.ApplicationServices.API.Domain.Models;
+using MovieManagement.ApplicationServices.API.ErrorHandling;
 using MovieManagement.DataAccess.CQRS;
 using MovieManagement.DataAccess.CQRS.Commands;
 
@@ -23,6 +24,11 @@ public class AddMovieHandler : IRequestHandler<AddMovieRequest, AddMovieResponse
         var movie = _mapper.Map<DataAccess.Entities.Movie>(request);
         var command = new AddMovieCommand { Parameter = movie };
         var entityMovie = await _commandExecutor.Execute(command);
+        if(entityMovie.Id is 0)
+        {
+            return new AddMovieResponse { Error = new ErrorModel(ErrorType.ValidationError)};
+        }
+
         var domainMovie = _mapper.Map<Movie>(entityMovie);
         var response = new AddMovieResponse { Data = domainMovie };
         return response;
--- MovieManagement/MovieManagement.DataAccess/CQRS/Commands/AddMovieCommand.cs
+++ MovieManagement/MovieManagement.DataAccess/CQRS/Commands/AddMovieCommand.cs
@@ -1,4 +1,5 @@
-﻿using MovieManagement.DataAccess.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using MovieManagement.DataAccess.Entities;
 
 namespace MovieManagement.DataAccess.CQRS.Commands;
 
@@ -6,6 +7,13 @@ public class AddMovieCommand : CommandBase<Movie, Movie>
 {
     public override async Task<Movie> Execute(MovieManagementStorageContext context)
     {
+        var movie = await context.Movies.FirstOrDefaultAsync(x => x.Title == Parameter!.Title && x.Year == Parameter!.Year);
+        if(movie is not null)
+        {
+            Parameter!.Id = 0;
+            return Parameter;
+        }
+
         await context.Movies.AddAsync(Parameter!);
         await context.SaveChangesAsync();
         return Parameter!;
