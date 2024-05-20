bd12b6c4c4d740509b4d462517cf43e93efe6ac0
Greg Zieliński <120790228+Zetix7@users.noreply.github.com>
2024-05-20 14:27:16 +0200

    Created UpdateMyPasswordRequestValidator class

---
+++ MovieManagement/MovieManagement.ApplicationServices/API/Validators/UpdateMyPasswordRequestValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using MovieManagement.ApplicationServices.API.Domain;
+
+namespace MovieManagement.ApplicationServices.API.Validators;
+
+public class UpdateMyPasswordRequestValidator : AbstractValidator<UpdateMyPasswordRequest>
+{
+    public UpdateMyPasswordRequestValidator()
+    {
+        RuleFor(x => x.Password).Length(10, 32).WithMessage("WRONG_LENGTH_(CORRECT_BETWEEN_10_AND_32)");
+    }
+}
